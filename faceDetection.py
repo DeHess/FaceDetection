0607a5fdc775f36543ad071b67fb627c98a82557
hessnat1 <hessnat1@students.zhaw.ch>
2024-12-02 13:42:40 +0100

    verify

--- faceDetection.py
+++ faceDetection.py
@@ -2,7 +2,16 @@ from deepface import DeepFace
 
 result = DeepFace.verify(
   img1_path = "gf1.jpg",
-  img2_path = "gf2komma5.jpg",
+  img2_path = "gf4.jpg",
+  enforce_detection=False
 )
 
-print(str(result))
+#print(str(result))
+
+
+dfs = DeepFace.find(
+  img_path = "gf1.jpg",
+  db_path = "C:/Users/nathh/GitHub/FaceDetection",
+)
+
+print(dfs)
